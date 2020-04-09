629201138ac231e5cd95838832029b1c0263e451
merlos <merlos@users.noreply.github.com>
2020-04-09 19:27:54 -0400

    npm install babel-loader and added to config loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,16 @@ module.exports = {
     example: './examples/index.js',
   },
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] },
+      },
+    ],
+  },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
