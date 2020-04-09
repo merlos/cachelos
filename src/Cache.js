81058adce05bdb21aeec7dfb11453c9392e5a5f1
merlos <merlos@users.noreply.github.com>
2020-04-09 18:34:21 -0400

    moved lib to src

--- src/Cache.js
+++ src/Cache.js
@@ -1,8 +1,7 @@
-
 /**
- * A simple local storage cache for JSON objects  
+ * A simple local storage cache for JSON objects
  */
-const Cache = ({validTime}) => { 
+const Cache = ({ validTime }) => {
   const cache = {}
   /**
    * Cache configuration
@@ -10,9 +9,9 @@ const Cache = ({validTime}) => {
   cache.config = {
     /**
      * Time an item is valid by default.
-     * Expressed in milliseconds 
+     * Expressed in milliseconds
      */
-    validTime: validTime ?? (3600 * 1000) //
+    validTime: validTime ?? 3600 * 1000, //
   }
 
   cache.expiresOnKey = (url) => {
@@ -20,21 +19,22 @@ const Cache = ({validTime}) => {
   }
   /**
    * Returns the time the `url` expires
-   * If the url does not exist returns null. 
-   */  
+   * If the url does not exist returns null.
+   */
+
   cache.getExpiresOn = (url) => {
     const expiresOn = localStorage.getItem(cache.expiresOnKey(url))
-    return  expiresOn ? parseInt(expiresOn) : null 
+    return expiresOn ? parseInt(expiresOn) : null
   }
 
   /**
    * Is the entrance of the time valid
    */
   cache.isValid = (url) => {
-    const expiresOn = cache.getExpiresOn(url) 
+    const expiresOn = cache.getExpiresOn(url)
     // If expiration time exists compare with current time
     // If does not exist it has expired.
-    return expiresOn ? (expiresOn > Date.now()) : false
+    return expiresOn ? expiresOn > Date.now() : false
   }
 
   /**
@@ -42,17 +42,19 @@ const Cache = ({validTime}) => {
    */
   cache._setExpiresOn = (url, expiresOn = -1) => {
     const defaultTime = Date.now() + cache.config.validTime
-    localStorage.setItem(cache.expiresOnKey(url),
-      (expiresOn < 0) ? defaultTime : expiresOn ) 
+    localStorage.setItem(
+      cache.expiresOnKey(url),
+      expiresOn < 0 ? defaultTime : expiresOn
+    )
   }
 
   /**
    * Adds or updates the url item in the localStorage
-   * 
+   *
    * expiresOn is expressed in milliseconds. If negative uses the default config.
-   * Example const expires = Date.now().getTime() + 1000 
+   * Example const expires = Date.now().getTime() + 1000
    */
-  cache.set =  (url, data, expiresOn = -1 ) => {
+  cache.set = (url, data, expiresOn = -1) => {
     //Adds/Update expiration time
     cache._setExpiresOn(url, expiresOn)
     //add/Update data
