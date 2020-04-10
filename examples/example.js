16bb568ccf7991f82b9374b1be88155400b63b94
merlos <merlos@users.noreply.github.com>
2020-04-10 16:30:33 -0400

    Implemented  examples of usage

--- examples/example.js
+++ examples/example.js
@@ -1,3 +1,101 @@
 import Cache from '../src/Cache'
 
-console.log('imported with success')
+/**
+ * Basic example
+ */
+export const example1 = async () => {
+  const cache = Cache()
+  const url = './users.json'
+  try {
+    console.log('==== example 1 ========')
+    console.log('1) A fetch that makes a request to server')
+    const users = await cache.fetch(url)
+    console.log('   Response:', users) // an array with users
+    console.log('   isValid', cache.isValid(url)) // true
+    console.log('   expiresOn:', Date.parse(cache.getExpiresOn()))
+
+    console.log('2) Second fetch gets JSON from cache, not from server')
+    const users2 = await cache.fetch(url)
+    console.log('   Response:', users2) // an array with users
+  } catch (error) {
+    console.log(error)
+  }
+  //Clear cache
+  cache.clear()
+}
+
+//Sleep function
+function sleep(sec) {
+  return new Promise((resolve) => setTimeout(resolve, sec * 1000))
+}
+
+const example2 = async () => {
+  try {
+    const config = {
+      name: 'MyCacheName', //If you need have more than one cache at a time, set a name
+      expiresAfter: 5, // Time an item in the cache will persist as valid in ms}
+    }
+    console.log('==== example 2  ========')
+    console.log(`Cache items will expire after ${config.expiresAfter} sec`)
+    const cache = Cache(config)
+    const url = './users.json'
+
+    console.log('1) A fetch that makes a request to server')
+    const users = await cache.fetch(url)
+    console.log('   isValid? ', cache.isValid(url)) // true
+    console.log('   expiresOn:', Date(cache.getExpiresOn()))
+
+    console.log('2) Waiting 6 seconds:')
+    await sleep(6)
+    console.log('   done waiting')
+    console.log('   isValid? ', cache.isValid(url)) // false
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const example3 = async () => {
+  try {
+    const config = {
+      expiresAfter: 5, // Time an item in the cache will persist as valid in ms}
+    }
+    console.log('==== example 3 ========')
+    console.log(`Cache items will expire after ${config.expiresAfter} ms`)
+    const cache = Cache(config)
+    const url = './users.json'
+
+    // If you want to have a lower level control of the cache
+    // you can create your own fetch
+    const myCacheFetch = async (url) => {
+      if (cache.isValid(url)) {
+        console.log('myCacheFetch: data from cache')
+        return cache.get(url)
+      }
+      console.log('myCacheFetch: data from server')
+      const res = await fetch(url)
+      if (res.ok) {
+        const data = await res.json()
+        //save to cache
+        cache.set(url, data)
+        return data
+      }
+      throw Error(res.errorText)
+    }
+
+    console.log('1) myCacheFech fetch ')
+    const users = await myCacheFetch(url)
+    console.log('2) Waiting 6 seconds:')
+    await sleep(6)
+    console.log('   done waiting')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const runExamples = async () => {
+  await example1()
+  await example2()
+  await example3()
+}
+
+runExamples().then('finished running example')
--- src/Cache.js
+++ src/Cache.js
@@ -1,7 +1,20 @@
 /**
  * A simple local storage cache for JSON objects
+ *
+ * Configuration options are
+ * ```json
+ *   {
+ *     expiresAfter: 3600 , // is the time an entry in the cache will be valid expressed in seconds
+ *     name: "@merlos/cachelos" // is the name of the cache. It is used to identify the items in the local storage that belong to the cache
+ *  }
+ * ```
+ * @param {Object} config optional configuration object
+ *
+ * @returns {Cache} A cache object
  */
-const Cache = ({ validTime }) => {
+const Cache = (config = {}) => {
+  const { expiresAfter, name } = config
+
   const cache = {}
   /**
    * Cache configuration
@@ -9,19 +22,32 @@ const Cache = ({ validTime }) => {
   cache.config = {
     /**
      * Time an item is valid by default.
-     * Expressed in milliseconds
+     * Expressed in seconds
      */
-    validTime: validTime ?? 3600 * 1000,
+    expiresAfter: expiresAfter ?? 3600,
+    name: name ?? '@merlos/cachelos',
   }
 
-  cache.expiresOnKey = (url) => {
-    return url + '--expiresOn'
+  /**
+   * Gets the key in local storage for the url
+   */
+  cache.itemKey = (url) => {
+    return `${cache.config.name}--${url}`
   }
   /**
-   * Returns the time the `url` expires
-   * If the url does not exist returns null.
+   * Gets the key in local storage with the expiration time for the url
+   *
    */
+  cache.expiresOnKey = (url) => {
+    return `${cache.itemKey(url)}--expiresOn`
+  }
 
+  /**
+   * Returns the time (EPOC) when the `url` is expected to expire
+   * If the url does not exist returns null.
+   *
+   * @returns {number} Number of milliseconds (EPOC)
+   */
   cache.getExpiresOn = (url) => {
     const expiresOn = localStorage.getItem(cache.expiresOnKey(url))
     return expiresOn ? parseInt(expiresOn) : null
@@ -41,7 +67,7 @@ const Cache = ({ validTime }) => {
    * Sets expiration time for the `url`
    */
   cache._setExpiresOn = (url, expiresOn = -1) => {
-    const defaultTime = Date.now() + cache.config.validTime
+    const defaultTime = Date.now() + cache.config.expiresAfter * 1000
     localStorage.setItem(
       cache.expiresOnKey(url),
       expiresOn < 0 ? defaultTime : expiresOn
@@ -58,14 +84,48 @@ const Cache = ({ validTime }) => {
     //Adds/Update expiration time
     cache._setExpiresOn(url, expiresOn)
     //add/Update data
-    localStorage.setItem(url, JSON.stringify(data))
+    localStorage.setItem(cache.itemKey(url), JSON.stringify(data))
   }
   /**
    *  Get item from cache
+   * @param {string} url
+   * @returns {Object} Parsed json object
    */
   cache.get = (url) => {
-    return JSON.parse(localStorage.getItem(url))
+    return JSON.parse(localStorage.getItem(cache.itemKey(url)))
+  }
+  cache.remove = (url) => {
+    localStorage.removeItem(cache.itemKey(url))
+  }
+
+  /**
+   * Fetch call that
+   */
+  cache.fetch = async (url, options) => {
+    if (cache.isValid(url)) {
+      return cache.get(url)
+    } else {
+      const res = await fetch(url, options)
+      if (res.ok) {
+        const data = await res.json()
+        cache.set(url, data)
+        return data
+      } else {
+        throw Error(res.statusText)
+      }
+    }
+  }
+  /**
+   * Clears the cache
+   */
+  cache.clear = async () => {
+    const keys = Object.keys(localStorage)
+    keys.map((key) => {
+      console.log('clear key:', key, key.includes(cache.config.name))
+      key.includes(cache.config.name) && localStorage.removeItem(key)
+    })
   }
   return cache
 }
+
 export default Cache
